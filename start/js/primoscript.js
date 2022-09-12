5cbd2534187031d2524914db8c4a892510e84d11
giox367 <111007060+Giox367@users.noreply.github.com>
2022-09-12 14:57:42 +0200

    Esercizio

---
+++ start/js/primoscript.js
@@ -0,0 +1,12 @@
+function scegliUnTitolo(){
+    var nuovoTitolo;
+    var titolo = prompt("Inscrisci un titolo decente");
+    
+    if (titolo != "Home" || titolo != "Homepage")
+    
+   alert("Ho detto un titolo decente");
+
+    else
+    document.getElementById("titologiusto").innerHTML = titolo;
+    
+}
