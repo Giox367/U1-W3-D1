82507516b47453d18e0429d8201d658c5ee5fa13
giox367 <111007060+Giox367@users.noreply.github.com>
2022-09-12 16:46:51 +0200

    ESERCIZIO

--- start/js/primoscript.js
+++ start/js/primoscript.js
@@ -2,11 +2,11 @@ function scegliUnTitolo(){
     
     var titolo = prompt("Inscrisci un titolo decente");
     
-    if (titolo !== "Home" && titolo !== "Homepage")
+    if (titolo !== "Home" && titolo !== "Homepage" && titolo !== "Giovanni")
     
    alert("Ho detto un titolo decente");
 
     else
-    document.getElementById("titologiusto").innerHTML = titolo;
+    document.getElementById("titologiusto").innerHTML =  titolo;
     
 }
